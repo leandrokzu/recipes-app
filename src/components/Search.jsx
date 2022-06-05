f69e200736fb7f820cb562ae30055101d4e9f71a
Leandro Augusto <leandroaugusto470@outlook.com>
2022-06-05 19:12:20 -0300

    projeto conclúido

---
+++ src/components/Search.jsx
@@ -0,0 +1,53 @@
+import styled from 'styled-components';
+import { useState } from 'react';
+import { FaSearch } from 'react-icons/fa';
+import { useNavigate } from 'react-router-dom';
+
+function Search() {
+  const [input, setInput] = useState('');
+  const navigate = useNavigate();
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+    navigate('/searched/' + input);
+  };
+  return (
+    <FormStyle onSubmit={submitHandler}>
+      <div>
+        <FaSearch />
+        <input
+          onChange={(e) => setInput(e.target.value)}
+          type="text"
+          value={input}
+        />
+      </div>
+    </FormStyle>
+  );
+}
+
+const FormStyle = styled.form`
+  margin: 0 20rem;
+  div {
+    position: relative;
+    width: 100%;
+  }
+  input {
+    width: 100%;
+    border: none;
+    outline: none;
+    background: linear-gradient(35deg, #494949, #313131);
+    font-size: 1.5rem;
+    color: white;
+    padding: 1rem 3rem;
+    border-radius: 1rem;
+  }
+  svg {
+    position: absolute;
+    color: white;
+    top: 50%;
+    left: 0%;
+    transform: translate(100%, -50%);
+  }
+`;
+
+export default Search;
